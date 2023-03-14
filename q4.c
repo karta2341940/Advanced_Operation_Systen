5aa4f668b4a81a020383e47a30827a414770d9bc
= <=>
2023-03-15 01:46:00 +0800

    UPDATE

--- q4.c
+++ q4.c
@@ -2,61 +2,97 @@
 Write a program that calls fork() and then calls some form of
 exec() to run the program /bin/ls. See if you can try all of the
 variants of exec(), including (on Linux) execl(), execle(),
-execlp(), execv(), execvp(), and execvpe(). Why do
-you think there are so many variants of the same basic call?
+execlp(), execv(), execvp(), and execvpe().
+Why do you think there are so many variants of the same basic call?
 */
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
+#include <unistd.h>
+#include <string.h>
 
-void proc(int);
+void proc(int, char *[], int);
 void err();
-void child();
+void child(char *[], int);
 void parent();
-/// @brief To handle the signal event
-/// @param sig 
-void signalHandler(int sig)
-{
-    sig=sig;
-    return ;
-}
 
-int main()
+const char *path = "/bin/ls";
+int main(int argc, char *argv[])
 {
-
-    signal(SIGCHLD, signalHandler);
+    printf("The argc is %d\n", argc);
     int rc = fork();
-    proc(rc);
+    proc(rc, argv, argc);
 }
-void proc(int rc)
+void proc(int rc, char *argv[], int argc)
 {
     if (rc < 0)
         err();
     if (rc)
     {
-        pause();
         parent();
+        wait(NULL);
     }
     else
     {
-        child();
+        child(argv, argc);
     }
 }
 void err()
 {
     printf("Something wrong\n");
-    return;
+    exit(1);
 }
 
 void parent()
 {
-    printf("goodbye\n");
+    printf("Parent\n");
 }
-void child()
+void child(char *argv[], int argc)
 {
-    printf("hello\n");
+    printf("Child\n");
+    int i = 0;
+    for (i = 0; i < argc; i++)
+    {
+        
+        printf("%d - ARG : %s\n", i,argv[i]);
+        if (strcmp(argv[i], "execl")==0)
+        {
+            printf("execl\n");
+            execl(path, "ls", "-al", NULL);
+            return 0;
+        }
+        if (!strcmp(argv[i], "execlp"))
+        {
+            printf("execlp\n");
+            execlp("ls", "ls", "-al", NULL);
+            return 0;
+        }
+        if (!strcmp(argv[i], "execle"))
+        {
+            printf("execle\n");
+            execle(path, "ls", "-al", NULL,NULL);
+            return 0;
+        }
+        if (!strcmp(argv[i], "execv"))
+        {
+            printf("execv\n");
+            const char *av[] = {"ls", "-l", NULL};
+            execv(path, av);
+            return 0;
+        }
+        if (!strcmp(argv[i], "execvp"))
+        {
+            printf("execvp\n");
+            const char *av[] = {"ls", "-l", NULL};
+            execvp("ls", av);
+            return 0;
+        }
+        if (!strcmp(argv[i], "execvpe"))
+        {
+            printf("execvp\n");
+            const char *av[] = {"ls", "-l", NULL};
+            execvpe("ls", av,NULL);
+            return 0;
+        }
+    }
 }
--- q5.c
+++ q5.c
@@ -3,3 +3,52 @@ Now write a program that uses wait() to wait for the child process
 to finish in the parent. What does wait() return? What happens if
 you use wait() in the child?
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+void proc(int);
+void err();
+void child();
+void parent();
+
+
+int main(int argc, char *argv[])
+{
+    int rc = fork();
+    proc(rc);
+}
+void proc(int rc)
+{
+    if (rc < 0)
+        err();
+    if (rc)
+    {
+        parent();
+        wait(NULL);
+    }
+    else
+    {
+        
+    }
+}
+void err()
+{
+    printf("Something wrong\n");
+    exit(1);
+}
+void parent()
+{
+    printf("Parent\n");
+}
+void child(char *argv[], int argc)
+{
+   
+}
+
+
+
+
