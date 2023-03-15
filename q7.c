5f4138cc073ecb852e4aa8fe94f5705994163101
MX,Du <38270318+karta2341940@users.noreply.github.com>
2023-03-15 07:09:36 +0000

    q7 finish

--- q7.c
+++ q7.c
@@ -1,5 +1,67 @@
 /*
-Write a program that creates a child process, and then in the child
-closes standard output (STDOUT FILENO). What happens if the child
-calls printf() to print some output after closing the descriptor?
+Write a program that creates a child process,
+and then in the child closes standard output
+(STDOUT FILENO). What happens if the child
+calls printf() to print some output after
+closing the descriptor?
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+void proc(int);
+void err();
+void parent();
+void child();
+
+int main()
+{
+    int pid = fork();
+    proc(pid);
+    if (pid == getpid())
+    {
+        printf("\n---------------Program END---------------\n");
+    }
+}
+void proc(int pid)
+{
+    switch (pid)
+    {
+    case 0:
+        child();
+        break;
+    case -1:
+        err();
+        break;
+
+    default:
+        parent();
+        break;
+    }
+}
+void err()
+{
+    printf("Error");
+    exit(-1);
+}
+
+void parent()
+{
+    printf("(Parent)I am parent\n");
+    wait(NULL);
+    printf("(Parent)Parent END\n");
+}
+void child()
+{
+    printf("(Child)Before CLOSE\n");
+    close(STDOUT_FILENO);
+    open("q7.txt", O_RDWR | O_TRUNC | O_CREAT, S_IRWXU);
+    printf("(Child)After CLOSED\n");
+    printf("(Child)Child END\n");
+}
--- q8.c
+++ q8.c
@@ -1,6 +1,21 @@
 /*
 Write a program that creates two children,
 and connects the standard output of one to
-the standard input of the other,using the 
+the standard input of the other,using the
 pipe() system call.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+int main()
+{
+    int pid1 = fork();
+    int pid2 = fork();
+}
