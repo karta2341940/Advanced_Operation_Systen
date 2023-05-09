e3c9c67a300193238122d01febb76515aff87d0b
MX,Du <a08487a@gmail.com>
2023-05-10 00:14:29 +0800

    Update Hw3

---
+++ hw3/null.c
@@ -0,0 +1,19 @@
+/*
+First, write a simple program called null.c that creates a pointer
+to an integer, sets it to NULL, and then tries to dereference it.
+Compile this into an executable called null. What happens when you
+run this program?
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *p = NULL;
+    printf("Start\n");
+    printf("The address of p is %p\n", p);
+    printf("The value of p is %d\n", *p);
+    printf("End\n");
+    return 0;
+}
